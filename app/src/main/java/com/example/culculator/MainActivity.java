d18a049f78ac60ba16e8dd20931e0154969a1635
SweeetR <rt7262@bs.amalnet.k12.il>
2019-03-06 21:08:53 +0200

    I Fixed all the issues

--- app/src/main/java/com/example/culculator/MainActivity.java
+++ app/src/main/java/com/example/culculator/MainActivity.java
@@ -5,76 +5,126 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     int sign;
-    double a,b,answer;
+    double a,b,answer,tempans;
     EditText et;
     String temp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        answer = 0;
         et = (EditText) findViewById(R.id.et);
-        temp ="";
+        sign = 1;
+        a = 0;
+        b = 0;
+        answer = 0;
+        temp = "";
+        et.setText("");
+
     }
 
     public void veod(View view) {
-        a = Double.parseDouble(et.getText().toString());
-        sign = 1 ;
-        et.setText("");
+        if (et.getText().toString().equals("")) {
+            et.setText("");
+            Toast.makeText(this, "Enter a number first", Toast.LENGTH_SHORT).show();
+        } else {
+            a = Double.parseDouble(et.getText().toString());
+            sign = 1;
+            et.setText("");
+        }
     }
 
     public void pahot(View view) {
-        a = Double.parseDouble(et.getText().toString());
-        sign = 2 ;
-        et.setText("");
+        if (et.getText().toString().equals("")) {
+            et.setText("");
+            Toast.makeText(this, "Enter a number first", Toast.LENGTH_SHORT).show();
+        } else {
+            a = Double.parseDouble(et.getText().toString());
+            sign = 2;
+            et.setText("");
+        }
     }
 
     public void caful(View view) {
-        a = Double.parseDouble(et.getText().toString());
-        sign = 3 ;
-        et.setText("");
+        if (et.getText().toString().equals("")) {
+            et.setText("");
+            Toast.makeText(this, "Enter a number first", Toast.LENGTH_SHORT).show();
+        } else {
+            a = Double.parseDouble(et.getText().toString());
+            sign = 3;
+            et.setText("");
+        }
     }
 
     public void helkei(View view) {
-        a = Double.parseDouble(et.getText().toString());
-        sign = 4 ;
-        et.setText("");
+        if (et.getText().toString().equals("")) {
+            et.setText("");
+            Toast.makeText(this, "Enter a number first", Toast.LENGTH_SHORT).show();
+        } else {
+            a = Double.parseDouble(et.getText().toString());
+            sign = 4;
+            et.setText("");
+        }
     }
 
     public void reset(View view) {
         a = 0;
         b = 0;
+        answer = 0;
+        temp = "";
         et.setText("");
     }
 
     public void shavee(View view) {
+        if(et.getText().toString().equals("")){
+        et.setText("");
+            Toast.makeText(this, "Enter a number first", Toast.LENGTH_SHORT).show();
+        }
+        else{
         b = Double.parseDouble(et.getText().toString());
-        switch(sign){
-            case 1:answer = a+b;
-            temp =temp+answer;
-            et.setText(temp);break;
-            case 2:answer = a-b;
-            temp =temp+answer;
-            et.setText(temp);break;
-            case 3:answer = a*b;
-            temp =temp+answer;
-            et.setText(temp);break;
-            case 4:answer = a/b;
-            temp =temp+answer;
-            et.setText(temp);break;
-
+        switch(sign) {
+            case 1:
+                answer = a + b;
+                temp = temp + answer;
+                et.setText(temp);
+                temp = "";
+                tempans = answer;
+                break;
+            case 2:
+                answer = a - b;
+                temp = temp + answer;
+                et.setText(temp);
+                temp = "";
+                tempans = answer;
+                break;
+            case 3:
+                answer = a * b;
+                temp = temp + answer;
+                et.setText(temp);
+                temp = "";
+                tempans = answer;
+                break;
+            case 4:
+                answer = a / b;
+                temp = temp + answer;
+                et.setText(temp);
+                temp = "";
+                tempans = answer;
+                break;
+        }
         }
     }
 
 
     public void go(View view) {
         Intent si = new Intent(this,CreditScreen.class);
-        si.putExtra("n",answer);
+        si.putExtra("n",tempans);
         startActivity(si);
     }
 }
